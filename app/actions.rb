e73d06e59b9f7f509f9dd1886386271ba8e4d319
SaraVRinCA <sara@ridebooker.com>
2017-12-11 02:31:14 +0000

    w5d2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 
 get "/" do
     @posts = Post.order(created_at: :desc)
@@ -26,4 +32,62 @@ post "/signup" do
     end
 end
 
-   
+get "/login" do
+    erb(:login)
+end
+
+post "/login" do  # when a GET resquest comes into /login
+    username = params[:username]
+    password = params[:password]
+    
+    # 1.find user by username
+    user =  User.find_by(username: username)
+
+    # 2. if user exist
+    
+    if user && user.password ==  password
+        session[:user_id] = user.id
+        # "Success! User with id #{session[:user_id]} is logged in!"
+        redirect to ("/")
+    else
+        @error_message = "Login failed"
+        erb(:login)
+    end
+end
+
+get "/logout" do
+    session[:user_id] = nil
+    # "Logout successful!"
+    redirect to ("/")
+end
+
+get "/" do
+    @posts = Post.order(created_at: :desc)
+    @current_user = User.find_by(id: session[:user_id])
+    erb(:index)
+end
+
+get "/posts/new" do
+    @post = Post.new
+    erb(:"posts/new")
+end
+
+post "/posts" do
+    photo_url = params[:photo_url]
+    
+    # instantiate new post
+    @post = Post.new({photo_url: photo_url, user_id: current_user.id})
+    
+    #if post is valid
+    if @post.save
+        redirect(to("/"))
+    else
+        #if it doesn´t validate
+       erb(:"posts/new")
+    end
+end
+
+get "/posts/:id" do
+    @post = Post.find(params[:id]) #fint the post with the id in URL
+    erb(:"posts/show") # render app/view/posts/show.erb
+end
